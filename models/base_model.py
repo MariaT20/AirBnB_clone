3e020e1b638bfd5e1cb5482ad26279c6818213a7
Blessing56 <blessingnwalozie@gmail.com>
2022-10-31 17:04:09 +0000

    corrections

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 
 import uuid
 from datetime import datetime
+import models
 
 """The BaseModel"""
 
@@ -20,6 +21,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """should print:[<class name>] (<self.id>) <self.__dict__>"""
@@ -28,6 +30,8 @@ class BaseModel:
     def save(self):
         """Update the attr updated_at with current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
+        return
 
     def to_dict(self):
         """Returns a dictionary containing all key/values of __dict__"""
