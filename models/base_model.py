c747fa9254acd86af4a06f7482632bfb95194cd6
Blessing56 <blessingnwalozie@gmail.com>
2022-10-31 16:03:57 +0000

    corrections

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,11 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                    setattr(self, key, value)
+                if key == "__class__":
+                    continue
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
